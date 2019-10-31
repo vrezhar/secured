0318877c0856ac90421d485b7c7e1252fb34b94c
vrezhar <vrarakel01@gmail.com>
2019-10-31 18:05:27 +0400

    document serialization

--- grails-app/services/com/ttreport/Base64EncoderService.groovy
+++ grails-app/services/com/ttreport/Base64EncoderService.groovy
@@ -1,41 +1,14 @@
 package com.ttreport
 
 import com.ttreport.data.Document
-import com.ttreport.logs.DevCycleLogger
 import grails.gorm.transactions.Transactional
-import groovy.json.JsonBuilder
 
 @Transactional
 class Base64EncoderService {
 
     String serializeAsJson(Document document)
     {
-        Map map = [
-                document_number: document.documentNumber,
-                request_type: document.requestType,
-                products: document.barCodes.collect{
-                    Map collected = [:]
-                    collected.tax = it.products.tax
-                    collected.cost = it.products.cost
-                    collected.description = it.products.description
-                    if(it.uit_code){
-                        collected.uit_code = it.uit_code
-                    }
-                    if(it.uitu_code){
-                        collected.uitu_code = it.uitu_code
-                    }
-                    collected
-                }
-        ]
-        if(document.requestType == "ACCEPTANCE"){
-
-        }
-        if(document.requestType == "SHIPMENT"){
-
-        }
-        def builder = new JsonBuilder(map)
-        DevCycleLogger.log("The document as json, 'prettified': ${builder.toPrettyString()}")
-        return builder.toString()
+       return Document.serializeAsJson(document)
     }
 
     String encodeAsBase64(Document document)
