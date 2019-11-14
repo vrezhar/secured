fafc4f81401c45fb41197a587ace95c4533dc2d6
vrezhar <vrarakel01@gmail.com>
2019-11-14 18:21:09 +0400

    Api refactoring, domain changes

---
+++ src/test/groovy/com/ttreport/data/documents/differentiated/ProductCirculationDocumentSpec.groovy
@@ -0,0 +1,19 @@
+package com.ttreport.data.documents.differentiated
+
+import com.ttreport.data.documents.differentiated.existing.ProductCirculationDocument
+import grails.testing.gorm.DomainUnitTest
+import spock.lang.Specification
+
+class ProductCirculationDocumentSpec extends Specification implements DomainUnitTest<ProductCirculationDocument> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
