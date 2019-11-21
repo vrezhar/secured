262c18eea1aa7e4cd796b590b7ae11f57c6d6024
vrezhar <vrarakel01@gmail.com>
2019-11-21 18:56:06 +0400

    Implementing command and document factories, starting api refactoring

---
+++ src/test/groovy/com/ttreport/data/documents/differentiated/GenericDocumentSpec.groovy
@@ -0,0 +1,18 @@
+package com.ttreport.data.documents.differentiated
+
+import grails.testing.gorm.DomainUnitTest
+import spock.lang.Specification
+
+class GenericDocumentSpec extends Specification implements DomainUnitTest<Document> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
